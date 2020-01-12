d3b1d543795a4ff0397e3c328d49a9f7303957d3
Håkan Fahlstedt <hf@fahlsoft.se>
2020-01-12 02:28:39 +0100

    Made hamburger menu work

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,32 +1,46 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useState } from "react";
 
-const Header = () => (
-    <nav className="navbar is-dark" role="navigation" aria-label="main navigation">
-        <div className="navbar-brand">
-            <Link href="/">
-                <a className="navbar-item title">Vad blir skatten?</a>
-            </Link>
-            <a role="button" className="navbar-burger burger" aria-label="menu" aria-expanded="false" data-target="navbarMain">
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-                <span aria-hidden="true"></span>
-            </a>
+const Header = () => {
+  const [menuIsActive, setMenuIsActive] = useState(false);
+
+  return (
+    <nav
+      className="navbar is-dark"
+      role="navigation"
+      aria-label="main navigation"
+    >
+      <div className="navbar-brand">
+        <Link href="/">
+          <a className="navbar-item title">Vad blir skatten?</a>
+        </Link>
+        <a
+          role="button"
+          className={`navbar-burger burger ${menuIsActive ? "is-active" : ""}`}
+          onClick={() => setMenuIsActive(!menuIsActive)}
+          aria-label="menu"
+          aria-expanded="false"
+          data-target="navbarMain"
+        >
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+          <span aria-hidden="true"></span>
+        </a>
+      </div>
+      <div id="navbarMain" className={`navbar-menu ${menuIsActive ? "is-active" : ""}`}>
+        <div className="navbar-start">
+          <Link href="/salary">
+            <a className="navbar-item">Endast lön</a>
+          </Link>
+          <Link href="/benefit">
+            <a className="navbar-item">Med förmån</a>
+          </Link>
+          <Link href="/raise">
+            <a className="navbar-item">Efter löneförhöjning</a>
+          </Link>
         </div>
-        <div id="navbarMain" className="navbar-menu">
-            <div className="navbar-start">
-                <Link href="/salary">
-                    <a className="navbar-item">Endast lön</a>
-                </Link>
-                <Link href="/benefit">
-                    <a className="navbar-item">Med förmån</a>
-                </Link>
-                <Link href="/raise">
-                    <a className="navbar-item">Efter löneförhöjning</a>
-                </Link>
-            </div>
-        </div>
+      </div>
     </nav>
   );
-  
-  export default Header;
-  
+};
+export default Header;
