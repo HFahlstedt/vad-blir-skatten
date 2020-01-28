7adc46261e932a33435b86c3e933c920faa8ac0b
Håkan Fahlstedt <hf@fahlsoft.se>
2020-01-28 15:08:25 +0100

    Added Google AdSense

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,10 +1,14 @@
+import Head from 'next/head';
 import Header from '../components/Header';
 
 import "../styles/main.scss";
 
 const Layout = props => (
   <div>
-      <Header/>
+    <Head>
+       <script data-ad-client="ca-pub-6251499797161261" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> 
+    </Head>
+    <Header/>
     <section className="section">
       <div className="container">
         <h1 className="title">{props.title}</h1>
