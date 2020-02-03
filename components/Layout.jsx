493dca55d549f5e8f5c82af2638aeaf36f17c36d
Håkan Fahlstedt <hf@fahlsoft.se>
2020-02-03 20:23:05 +0100

    Disable Google AdSense

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -5,9 +5,6 @@ import "../styles/main.scss";
 
 const Layout = props => (
   <div>
-    <Head>
-       <script data-ad-client="ca-pub-6251499797161261" async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script> 
-    </Head>
     <Header/>
     <section className="section">
       <div className="container">
